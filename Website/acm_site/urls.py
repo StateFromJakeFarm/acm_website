62eba127802f617bcf36f3ee10ec7b5bc1fb02a7
Jasper Stubbe <jstubbe1@mail.csuchico.edu>
2018-10-22 17:54:01 -0700

    rename website to be lowecase

--- Website/acm_site/urls.py
+++ Website/acm_site/urls.py
@@ -1,4 +1,4 @@
-"""ACM URL Configuration
+"""acm_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
