59c1d30597adc6436a41a3f724938d4c0fd31342
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-11-13 20:57:22 -0800

    use crispy forms to programmatically style forms with bootstrap4

--- Website/acm_app/forms.py
+++ Website/acm_app/forms.py
@@ -13,7 +13,7 @@ class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
--- Website/acm_site/settings.py
+++ Website/acm_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'acm_app',
 ]
 
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Crispy forms bootstrap version
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
