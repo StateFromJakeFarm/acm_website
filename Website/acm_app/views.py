e51c0e2149271e4ef4f5f55e66de45621081bda2
Jasper Stubbe <jstubbe1@mail.csuchico.edu>
2018-11-27 20:11:09 -0800

    Fix my CSS stuff

--- Website/acm_app/views.py
+++ Website/acm_app/views.py
@@ -1,7 +1,7 @@
 import os
 
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.template.defaultfilters import slugify
 from django.contrib.auth.decorators import login_required
 from django.core.files.storage import DefaultStorage
@@ -11,11 +11,13 @@ from . import forms
 from . import models
 from .helpers import store_uploaded_file, run_submission
 
-def login(request):
-    '''
-    Send user to login page
-    '''
-    return render(request, 'registration/login.html')
+
+# def login_view(request):
+#     '''
+#     Send user to login page
+#     '''
+#     return render(request, 'registration/login.html')
+
 
 def logout_view(request):
     '''
@@ -24,6 +26,7 @@ def logout_view(request):
     logout(request)
 
     return redirect('/')
+
 
 def register(request):
     '''
@@ -34,58 +37,82 @@ def register(request):
         registration_form = forms.RegistrationForm(request.POST)
         if registration_form.is_valid():
             registration_form.save(commit=True)
-            return redirect('/accounts/login/')
+            new_user = authenticate(username=registration_form.cleaned_data['username'],
+                                    password=registration_form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return redirect('/')
     else:
         # Present form to user
         registration_form = forms.RegistrationForm()
-    
+
     context = {
-        'form': registration_form
+        'form': registration_form,
+        'nbar' : "Register"
     }
 
     return render(request, 'registration/register.html', context=context)
+
 
 def home(request):
     '''
     Render homepage.
     '''
-    return render(request, 'home.html')
+    context = {
+        'nbar' : 'Home'
+    }
+
+    return render(request, 'home.html', context=context)
+
 
 def all_problems(request):
 
     context = {
-        'problems': models.ProblemModel.objects.all().order_by('-id')
+        'problems': models.ProblemModel.objects.all().order_by('-id'),
+        'nbar' : 'Problems'
     }
 
     return render(request, 'all_problems.html', context=context)
+
 
 def problem(request, slug=''):
 
     test_results = ''
 
     if request.method == 'POST':
-        submission_form = forms.ProblemSubmissionForm(request.POST, request.FILES)
+        submission_form = forms.ProblemSubmissionForm(
+            request.POST, request.FILES)
         if submission_form.is_valid():
-            local_file_path = store_uploaded_file(request.FILES['solution_file'], '/tmp')
+            local_file_path = store_uploaded_file(
+                request.FILES['solution_file'], '/tmp')
             problem = models.ProblemModel.objects.get(slug=slug)
-            testcases_path = os.path.join(settings.MEDIA_ROOT, str(problem.testcases))
-            test_results = run_submission(local_file_path, testcases_path).decode('utf-8').strip()
+            testcases_path = os.path.join(
+                settings.MEDIA_ROOT, str(problem.testcases))
+            test_results = run_submission(
+                local_file_path, testcases_path).decode('utf-8').strip()
     else:
         # Present form to user
         submission_form = forms.ProblemSubmissionForm()
 
     context = {
         'test_results': test_results,
-        'form': submission_form
+        'form': submission_form,
+        'nbar': 'Problems'
     }
 
     return render(request, 'problem.html', context=context)
+
 
 def leaderboard(request):
     '''
     TODO: Render leaderboard
     '''
-    return render(request, 'home.html')
+    context = {
+        'nbar': 'Leaderboard'
+    }
+
+    return render(request, 'home.html', context=context)
+
 
 @login_required
 def create_or_edit_problem(request):
@@ -97,11 +124,11 @@ def create_or_edit_problem(request):
         edit_form = forms.CreateOrEditProblemForm(request.POST, request.FILES)
         if edit_form.is_valid():
             edited = models.ProblemModel(
-                title = edit_form.cleaned_data['title'],
-                slug = slugify(edit_form.cleaned_data['title']),
-                description = edit_form.cleaned_data['description'],
-                author = request.user,
-                testcases = request.FILES['testcases']
+                title=edit_form.cleaned_data['title'],
+                slug=slugify(edit_form.cleaned_data['title']),
+                description=edit_form.cleaned_data['description'],
+                author=request.user,
+                testcases=request.FILES['testcases']
             )
             edited.save()
             return redirect('/problems')
@@ -110,7 +137,8 @@ def create_or_edit_problem(request):
         edit_form = forms.CreateOrEditProblemForm()
 
     context = {
-        'form': edit_form
+        'form': edit_form,
+        'nbar': 'Problems'
     }
 
     return render(request, 'edit.html', context=context)
