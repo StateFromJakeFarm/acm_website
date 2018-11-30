c350944ec995b8787bb23cb82dba2243a24668ed
Jasper Stubbe <jstubbe1@mail.csuchico.edu>
2018-11-30 11:12:12 -0800

    Skeleton Model For leaderboard

--- Website/acm_app/models.py
+++ Website/acm_app/models.py
@@ -10,3 +10,11 @@ class ProblemModel(models.Model):
     description = models.TextField()
     author      = models.ForeignKey(User, on_delete=models.PROTECT)
     testcases   = models.FileField(upload_to='testcases') # Expecting this to be a tarball of the .in and .out files
+
+class LeaderboardModel(models.Model):
+    '''
+    Store a relationship between users and solved porblems
+    '''
+    # one to one USER
+    # one to many ProblemModel
+    # int score
